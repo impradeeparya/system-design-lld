4b720c38446a410a0668861686b91421fbf9e26e
Pradeep Arya <impradeeparya@gmail.com>
2021-07-28 22:20:17 +0530

    Create StateMachineConstant.java

---
+++ service/src/lld/service/vendingmachine/constant/StateMachineConstant.java
@@ -0,0 +1,12 @@
+package lld.service.vendingmachine.constant;
+
+/**
+ * @author Pradeep Arya
+ */
+public class StateMachineConstant {
+
+    public static final String ITEM_LOCATION = "itemLocation";
+    public static final String ITEMS = "items";
+    public static final String ITEM_QUANTITY = "itemQuantity";
+
+}
